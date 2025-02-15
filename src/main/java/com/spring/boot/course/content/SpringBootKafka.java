4611c3a277b294473b04ba19c31f3b75f7b97316
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2025-02-15 09:43:46 +0530

    redis completed kafka started

---
+++ src/main/java/com/spring/boot/course/content/SpringBootKafka.java
@@ -0,0 +1,14 @@
+package com.spring.boot.course.content;
+
+public class SpringBootKafka {
+
+    // What is KAFKA ?
+
+    /*
+        Open source Distributed event streaming platform.
+        Apache Kafka is an open-source distributed event streaming platform designed
+        to handle high-throughput, low-latency data streams.
+
+
+    */
+}
