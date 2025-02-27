3cf7062992176a21330cd21067b803e899700c46
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2025-02-27 10:53:49 +0530

    reset token model and some config file changes

--- src/main/java/com/spring/boot/course/journalapp/entity/ResetToken.java
+++ src/main/java/com/spring/boot/course/journalapp/entity/ResetToken.java
@@ -1,8 +1,57 @@
 package com.spring.boot.course.journalapp.entity;
 
+import lombok.NonNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
 /**
  * @author prabhakar, @Date 26-02-2025
  */
 
+@Document
 public class ResetToken {
+
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    @NonNull
+    private String token;
+    @DBRef
+    private User user;
+
+    public ResetToken() {
+        super();
+    }
+
+    public ResetToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    public String getId() {
+        return UUID.randomUUID().toString();
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 }
--- src/main/java/com/spring/boot/course/journalapp/repository/ResetTokenRepository.java
+++ src/main/java/com/spring/boot/course/journalapp/repository/ResetTokenRepository.java
@@ -1,8 +1,16 @@
 package com.spring.boot.course.journalapp.repository;
 
+import com.spring.boot.course.journalapp.entity.ResetToken;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 /**
  * @author prabhakar, @Date 26-02-2025
  */
 
-public class ResetTokenRepository {
+@Repository
+public interface ResetTokenRepository extends MongoRepository<ResetToken, String> {
+    Optional<ResetToken> findByToken(String token);
 }
