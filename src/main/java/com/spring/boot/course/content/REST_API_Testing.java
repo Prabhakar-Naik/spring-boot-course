4060824874812c74cf13e4aceff344969436fa52
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2025-04-10 10:27:31 +0530

    API Testing Content updated

---
+++ src/main/java/com/spring/boot/course/content/REST_API_Testing.java
@@ -0,0 +1,51 @@
+package com.spring.boot.course.content;
+
+/**
+ * @author prabhakar, @Date 10-04-2025
+ */
+
+public class REST_API_Testing {
+
+    // What is API Testing?
+    /**
+     * API testing is a type of software testing that focuses on
+     * verifying the functionality, reliability, performance, and
+     * security of Application Programming Interfaces (APIs).
+     * Unlike traditional GUI testing.
+     * API testing involves sending requests to the API and validating the responses,
+     * without interacting with the user interface.
+     * */
+
+    // Key Principles of API Testing
+    /**
+     * API testing is crucial in modern software development as it ensures that
+     * different software systems can communicate and exchange data accurately
+     * and securely. It helps identify issues early in the development process,
+     * reducing the risk of bugs and improving the overall quality of the application.
+     */
+
+    // Types of API Testing
+    /**
+     * Functional Testing: Validates that the API functions as expected for various input conditions.
+     * Load Testing: Checks the API's performance under high traffic conditions.
+     * Security Testing: Ensures the API is protected against vulnerabilities.
+     * End-to-End Testing: Validates the entire workflow involving multiple API calls.
+     * Contract Testing: Ensures that the API adheres to the defined contract or specification
+     */
+
+    // HTTP STATUS CODE:
+    /**
+     * An HTTP status code is a three-digit numeric code returns by a web server as
+     * part of the response to an HTTP request made by a client.
+     * These status codes are used to convey information about the result or status
+     * of the requested operation.
+
+     Status Code Series:
+     *         100 - 199  ==> Informational response
+     *         200 - 299  ==> Successful response
+     *         300 - 399  ==> Redirection messages
+     *         400 - 499  ==> Client Error
+     *         500 - 599  ==> Server Error
+     */
+
+}
